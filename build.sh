8be0a6044773f6fa81b5a4b30f58c9c6622dec48
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-11-30 22:55:32 -0300

    Import upstream changes from OBGGCC

--- build.sh
+++ build.sh
@@ -280,6 +280,7 @@ if ! [ -f "${gcc_tarball}" ]; then
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0007-Add-relative-RPATHs-to-GCC-host-tools.patch"
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0008-Add-ARM-and-ARM64-drivers-to-OpenBSD-host-tools.patch" || true
 	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0009-Fix-missing-stdint.h-include-when-compiling-host-tools-on-OpenBSD.patch"
+	patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/gcc-15/0001-Enable-automatic-linking-of-libatomic.patch"
 fi
 
 # Follow Debian's approach to remove hardcoded RPATHs from binaries
